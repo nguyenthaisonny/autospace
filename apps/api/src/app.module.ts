769ee1bbef19357288d83e9d73c3a5f3910a93b2
Nguyễn Thái Son Ny <134358623+nguyenthaisonny@users.noreply.github.com>
2024-08-26 04:56:04 +0700

    add rest and graphql structure

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-
+import { GraphQLModule } from '@nestjs/graphql/dist/graphql.module'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
+import { ConfigModule } from '@nestjs/config';
+import { Prisma } from '@prisma/client'
+import { PrismaModule } from './common/prisma/prisma.module'
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      introspection: true,
+      fieldResolverEnhancers: ['guards'],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
